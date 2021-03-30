9d0ea9b596c0a20363249077d35b3ca42c23052d
Winston Sebesta <winston@sebesta.io>
2021-03-30 21:32:20 +0200

    chore: replace prettier and eslint with rome for simplicity

--- sample-application/webpack.config.js
+++ sample-application/webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const SimpleStopwatchPlugin = require('../index.js');
+const path = require("path");
+const SimpleStopwatchPlugin = require("../index.js");
 
 module.exports = {
-    mode: 'development',
-    entry: './sample-application/src/index.js',
-    plugins: [new SimpleStopwatchPlugin({ statsFolder: '.stats' })],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+	mode: "development",
+	entry: "./sample-application/src/index.js",
+	plugins: [new SimpleStopwatchPlugin({statsFolder: ".stats"})],
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
 };
