01bbabd95435180b7b45af25730a4669bad7ba30
Winston Sebesta <25691748+winstonthehun@users.noreply.github.com>
2021-03-31 21:24:23 +0200

    Revert "chore: replace prettier and eslint with rome for simplicity"
    This reverts commit f9208a65c5d3f0d20b830a630d9d20df5644f2ce.

--- sample-application/src/index.js
+++ sample-application/src/index.js
@@ -1,10 +1,10 @@
 function component() {
-	const element = document.createElement("div");
+    const element = document.createElement('div');
 
-	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = "<div>Hello fancy webpack example!</div>";
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = `<div>Hello fancy webpack example!</div>`;
 
-	return element;
+    return element;
 }
 
 document.body.appendChild(component());
