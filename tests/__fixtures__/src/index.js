aa7bf23a5e27576a7ac97800e2bbef66f23dca06
Winston Sebesta <winston@sebesta.io>
2021-04-19 21:22:22 +0200

    chore: rm old comment about lodash

--- tests/__fixtures__/src/index.js
+++ tests/__fixtures__/src/index.js
@@ -2,7 +2,6 @@ function component() {
     // eslint-disable-next-line no-undef
     const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = `<div>Hello fancy webpack example!</div>`;
 
     return element;
